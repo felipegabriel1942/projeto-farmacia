e04ce570469686d2ce6672ce1c88d1d38f1b7333
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-08-18 11:18:02 -0300

    Primeiro commit

---
+++ projeto-farmacia/src/main/java/br/com/farmacia/DAO/FornecedoresDAO.java
@@ -0,0 +1,93 @@
+package br.com.farmacia.DAO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import br.com.farmacia.domain.Fornecedores;
+import br.com.farmacia.factory.ConexaoFactory;
+
+public class FornecedoresDAO {
+
+	public void salvar(Fornecedores f) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("INSERT INTO fornecedores ");
+		sql.append("(descricao) ");
+		sql.append("VALUES (?)");
+
+		Connection conexao = ConexaoFactory.conectar();
+
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setString(1, f.getDescricao());
+		comando.executeUpdate();
+	}
+
+	public void excluir(Fornecedores f) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("DELETE FROM fornecedores ");
+		sql.append("WHERE codigo = ? ");
+
+		Connection conexao = ConexaoFactory.conectar();
+
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setLong(1, f.getCodigo());
+		comando.executeUpdate();
+	}
+
+	public void editar(Fornecedores f) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("UPDATE fornecedores ");
+		sql.append("SET descricao = ? ");
+		sql.append("WHERE codigo = ? ");
+
+		Connection conexao = ConexaoFactory.conectar();
+
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+
+		comando.setString(1, f.getDescricao());
+		comando.setLong(2, f.getCodigo());
+		comando.executeUpdate();
+	}
+
+	public Fornecedores buscaPorCodigo(Fornecedores f) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT codigo, descricao ");
+		sql.append("FROM fornecedores ");
+		sql.append("WHERE codigo = ? ");
+
+		Connection conexao = ConexaoFactory.conectar();
+
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+
+		comando.setLong(1, f.getCodigo());
+		ResultSet resultado = comando.executeQuery();
+		Fornecedores retorno = null;
+		
+		if(resultado.next()) {
+			retorno = new Fornecedores();
+			retorno.setCodigo(resultado.getLong("codigo"));
+			retorno.setDescricao(resultado.getString("descricao"));
+		}
+		
+		return retorno;
+	}
+
+	// Metodo de teste
+	public static void main(String[] args) {
+		Fornecedores f1 = new Fornecedores();
+		f1.setCodigo(1);
+		f1.setDescricao("FELIPE GABRIEL");
+		FornecedoresDAO fDao = new FornecedoresDAO();
+
+		try {
+			fDao.editar(f1);
+
+			System.out.println("Editado com sucesso");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Erro ao editar");
+		}
+
+	}
+}
