4b3ec773419d404fa00a26574010cdef46057ca6
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-08-19 20:18:54 -0300

    Metodo de listar e excluir produtos

--- projeto-farmacia/src/test/java/br/com/farmacia/test/ProdutoDAOTeste.java
+++ projeto-farmacia/src/test/java/br/com/farmacia/test/ProdutoDAOTeste.java
@@ -1,7 +1,9 @@
 package br.com.farmacia.test;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -10,9 +12,11 @@ import br.com.farmacia.domain.Fornecedores;
 import br.com.farmacia.domain.Produtos;
 
 
+
 public class ProdutoDAOTeste {
 	
 	@Test
+	@Ignore
 	public void salvar() throws SQLException {
 		Produtos p1 = new Produtos();
 		p1.setDescricao("DIPIRONA");
@@ -26,5 +30,21 @@ public class ProdutoDAOTeste {
 		ProdutoDAO pdao = new ProdutoDAO();
 		pdao.salvar(p1);
 		
+	}
+	
+	@Test
+	public void listar() throws SQLException {
+		ProdutoDAO fdao = new ProdutoDAO();
+		ArrayList<Produtos> lista = fdao.listar();
+		
+		for(Produtos p : lista) {
+			System.out.println("Código do Produto: " + p.getCodigo());
+			System.out.println("Descrição do Produto: " + p.getDescricao());
+			System.out.println("Valor do Produto: " + p.getPreco());
+			System.out.println("Quantidade: " + p.getQuantidade());
+			System.out.println("Código do Fornecedor: " + p.getFornecedor().getCodigo());
+			System.out.println("Descrição do Fornecedor: " + p.getFornecedor().getDescricao());
+			System.out.println("----------------------------------------------------------------------------");
+		}
 	}
 }
